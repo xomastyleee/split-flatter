2d71e4d8239efea052395db56c846583aff436cd
xomastyleee <vlad.khomenko@casavi.de>
2023-09-07 14:42:22 +0300

    changes for users module: empty

--- mock-server/src/app.module.ts
+++ mock-server/src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule],
 })
 export class AppModule {}
